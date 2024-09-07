35bde104a38eba078cd09195cd5255a59a735066
ArpanSankesh <arpansankesh830@gmail.com>
2024-09-07 16:49:01 +0530

    Valid Anagram

---
+++ ValidAnagram.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int[] arr = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            arr[s.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            arr[t.charAt(i) - 'a']--;
+            if (arr[t.charAt(i) - 'a'] < 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the first string: ");
+        String s = scanner.nextLine();
+        
+        System.out.print("Enter the second string: ");
+        String t = scanner.nextLine();
+        
+        Solution solution = new Solution();
+        boolean result = solution.isAnagram(s, t);
+
+        if (result) {
+            System.out.println("The strings are anagrams.");
+        } else {
+            System.out.println("The strings are not anagrams.");
+        }
+        
+        scanner.close();
+    }
+}
