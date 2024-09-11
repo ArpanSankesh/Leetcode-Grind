0e9c77f50b1548b0caa1f8f33bcac00747970849
ArpanSankesh <arpansankesh830@gmail.com>
2024-09-11 19:58:26 +0530

    Move Zeros

--- MoveZeroes.java
+++ MoveZeroes.java
@@ -18,29 +18,28 @@ class Solution {
     }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
         // Prompt the user to enter the size of the array
-        System.out.print("Enter the size of the array: ");
-        int size = scanner.nextInt();
-
-        // Create an array to store the input numbers
-        int[] nums = new int[size];
-
-        // Prompt the user to enter the array elements
-        System.out.println("Enter the elements of the array:");
-        for (int i = 0; i < size; i++) {
-            nums[i] = scanner.nextInt();
+        try (Scanner scanner = new Scanner(System.in)) {
+            // Prompt the user to enter the size of the array
+            System.out.print("Enter the size of the array: ");
+            int size = scanner.nextInt();
+            
+            // Create an array to store the input numbers
+            int[] nums = new int[size];
+            
+            // Prompt the user to enter the array elements
+            System.out.println("Enter the elements of the array:");
+            for (int i = 0; i < size; i++) {
+                nums[i] = scanner.nextInt();
+            }
+            
+            // Create an instance of the Solution class
+            Solution sol = new Solution();
+            // Call the moveZeroes method
+            sol.moveZeroes(nums);
+            
+            // Print the modified array
+            System.out.println("Array after moving zeroes to the end: " + Arrays.toString(nums));
         }
-
-        // Create an instance of the Solution class
-        Solution sol = new Solution();
-        // Call the moveZeroes method
-        sol.moveZeroes(nums);
-
-        // Print the modified array
-        System.out.println("Array after moving zeroes to the end: " + Arrays.toString(nums));
-
-        scanner.close();
     }
 }
