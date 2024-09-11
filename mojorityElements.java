5d4f8c9138f3a599461dbcca4e8134f3ee4a8b01
ArpanSankesh <arpansankesh830@gmail.com>
2024-09-11 19:58:12 +0530

    Majority Elements

--- mojorityElements.java
+++ mojorityElements.java
@@ -21,29 +21,28 @@ class Solution {
     }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
         // Prompt the user to enter the size of the array
-        System.out.print("Enter the size of the array: ");
-        int size = scanner.nextInt();
-
-        // Create an array to store the input numbers
-        int[] nums = new int[size];
-
-        // Prompt the user to enter the array elements
-        System.out.println("Enter the elements of the array:");
-        for (int i = 0; i < size; i++) {
-            nums[i] = scanner.nextInt();
+        try (Scanner scanner = new Scanner(System.in)) {
+            // Prompt the user to enter the size of the array
+            System.out.print("Enter the size of the array: ");
+            int size = scanner.nextInt();
+            
+            // Create an array to store the input numbers
+            int[] nums = new int[size];
+            
+            // Prompt the user to enter the array elements
+            System.out.println("Enter the elements of the array:");
+            for (int i = 0; i < size; i++) {
+                nums[i] = scanner.nextInt();
+            }
+            
+            // Create an instance of the Solution class
+            Solution sol = new Solution();
+            // Call the majorityElement method
+            int majority = sol.majorityElement(nums);
+            
+            // Print the majority element
+            System.out.println("The majority element is: " + majority);
         }
-
-        // Create an instance of the Solution class
-        Solution sol = new Solution();
-        // Call the majorityElement method
-        int majority = sol.majorityElement(nums);
-
-        // Print the majority element
-        System.out.println("The majority element is: " + majority);
-
-        scanner.close();
     }
 }
