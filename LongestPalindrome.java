8ad84e950fa6b0239c4c0eb18f811b8d1636dd0a
ArpanSankesh <arpansankesh830@gmail.com>
2024-09-12 16:06:02 +0530

    Longest Palindrome

--- LongestPalindrome.java
+++ LongestPalindrome.java
@@ -33,17 +33,15 @@ public class LongestPalindrome {
     }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        LongestPalindrome lp = new LongestPalindrome();
-        
-        // Prompt the user for input
-        System.out.print("Enter a string: ");
-        String userInput = scanner.nextLine();  // Read user input
-        
-        // Calculate and display the longest palindrome length
-        int result = lp.longestPalindrome(userInput);
-        System.out.println("The length of the longest palindrome that can be built is: " + result);
-        
-        scanner.close(); // Close the scanner
+        try (Scanner scanner = new Scanner(System.in)) {
+            LongestPalindrome lp = new LongestPalindrome();
+            // Prompt the user for input
+            System.out.print("Enter a string: ");
+            String userInput = scanner.nextLine();  // Read user input
+            // Calculate and display the longest palindrome length
+            int result = lp.longestPalindrome(userInput);
+            System.out.println("The length of the longest palindrome that can be built is: " + result);
+            // Close the scanner
+        }
     }
 }
