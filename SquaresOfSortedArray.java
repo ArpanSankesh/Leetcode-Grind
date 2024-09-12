b7e855c00caa011440e09af2e917c9a46c7699bc
ArpanSankesh <arpansankesh830@gmail.com>
2024-09-12 17:57:13 +0530

    977 Squares of sorted Array

---
+++ SquaresOfSortedArray.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+
+        int left = 0, right = n - 1, pos = n - 1;
+
+        while (left <= right) {
+            int leftSquare = nums[left] * nums[left];
+            int rightSquare = nums[right] * nums[right];
+
+            if (leftSquare > rightSquare) {
+                result[pos--] = leftSquare;
+                left++;
+            } else {
+                result[pos--] = rightSquare;
+                right--;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the number of elements in the array:");
+        int n = scanner.nextInt();
+        int[] nums = new int[n];
+
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = scanner.nextInt();
+        }
+
+        Solution solution = new Solution();
+        int[] sortedSquares = solution.sortedSquares(nums);
+
+        System.out.println("Sorted squares of the array:");
+        System.out.println(Arrays.toString(sortedSquares));
+    }
+}
